5a51ec241598cedc37fc360438cad495b9d92b7a
Vanessa <suede210810@gmail.com>
2024-10-24 20:51:01 +0200

    add script for axeptio

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Head from "next/head";
+import Footer from "../components/footer";
 import Hero from "../components/hero";
 import Navbar from "../components/navbar";
-import Footer from "../components/footer";
-
 
 import PopupWidget from "../components/popupWidget";
-import Stack from "../components/stack";
 import Projects from "../components/projects";
 import Recommendations from "../components/recommendations";
+import Stack from "../components/stack";
 
 const Home = () => {
   return (
@@ -16,21 +15,34 @@ const Home = () => {
         <title>Portfolio Vanessa G.</title>
         <meta name="description" content="Portoflio of Vanessa G." />
         <link rel="icon" href="/favicon.ico" />
+        {/* Script Axeptio */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.axeptioSettings = {
+                clientId: "671a9087ebecbc5f46f0af74", 
+              };
+
+              (function(d, s) {
+                var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+                e.async = true; e.src = "//static.axept.io/sdk.js";
+                t.parentNode.insertBefore(e, t);
+              })(document, "script");
+            `,
+          }}
+        />
       </Head>
 
       <Navbar />
       <Hero />
       <Stack />
-      <Recommendations/>
-        <Projects />
-    
-
-
+      <Recommendations />
+      <Projects />
 
       <Footer />
       <PopupWidget />
     </>
   );
-}
+};
 
 export default Home;
