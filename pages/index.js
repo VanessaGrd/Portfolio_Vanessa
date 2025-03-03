31d6bbe729d30f7c6aec52e5e046217d746f62cf
VanessaGrd <suede210810@gmail.com>
2025-03-03 19:34:27 +0100

    add analytics

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,15 @@ const Home = () => {
         <meta name="description" content="Portoflio of Vanessa G." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-1HN873YLB9"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-1HN873YLB9');
+      </script>
 
       <Navbar />
       <Hero />
